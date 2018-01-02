5d73cebc42a9ebea4ffeffcfbe090ea17f99be0c
chipsnow94 <ddphuvinh.1994@gmail.com>
2018-01-02 10:16:58 +0700

    harrybook v3

--- src/HarryPotter.php
+++ src/HarryPotter.php
@@ -2,124 +2,76 @@
 
 class HarryPotter
 {
-    private $set_of_book;
+    private $book_array;
 
     const PRICE_BOOK = 8;
-    const DISCOUNT = [
+    const DISCOUNT_RATE = [
             1 => 0,
             2 => 0.05,
             3 => 0.1,
             4 => 0.2,
             5 => 0.25
     ];
-    public function caculate($books)
+    public function price_Calculate($books)
     {
-        $totaldiscount = "";
+        $totaldiscount = [];
         $discountprice = 0;
-        $this->set_of_book = array_count_values($books);
-        $price=$this->defaulPrice();
-        $counttypes = $this->countType();
-        for (; $counttypes> 1; $counttypes = $this->countType()) {
-            // $totaldiscount .= (string)$counttypes;
-            $discountprice += $counttypes * self::PRICE_BOOK * self::DISCOUNT[$counttypes];
-            $this->updateValueTypes($counttypes);
+
+        $this->book_array = array_count_values($books);
+        $price = $this->default_Price();
+        $counttypes = $this->count_Book_Type();
+        // while ($counttypes>1) {
+        //     # code...
+        // }
+        for (; $counttypes> 1; $counttypes = $this->count_Book_Type()) {
+            $totaldiscount[]= $counttypes;
+            $discountprice += $counttypes * self::PRICE_BOOK * self::DISCOUNT_RATE[$counttypes];
+            $this->update_Value_Types($counttypes);
+
+            if (count($this->book_array) == 5) {
+                $three_discount_times = $this->count_Discount_Times($totaldiscount, 3);
+                $five_discount_times = $this->count_Discount_Times($totaldiscount, 5);
+                $diffent_price = min($three_discount_times, $five_discount_times)*0.4;
+                $discountprice += $diffent_price;
+            }
         }
-        
-        return (double)$price - $discountprice;
+        return (float)$price - $discountprice;
     }
-    private function updateValueTypes($counttypes)
+    private function update_Value_Types($counttypes)
     {
-        for ($i = 1; $i <= count($this->set_of_book);  $i++) {
-            if ($this->set_of_book[$i] >= 1) {
-                $this->set_of_book[$i]--;
+        for ($i = 1; $i <= count($this->book_array);  $i++) {
+            if ($this->book_array[$i] >= 1) {
+                $this->book_array[$i]--;
                 $counttypes--;
             }
         }
     }
-    private function countType()
+    private function count_Book_Type()
     {
         $counttypes = 0;
-        for ($i = 1; $i <= count($this->set_of_book); $i++) {
-            if ($this->set_of_book[$i] > 0) {
+        for ($i = 1; $i <= count($this->book_array); $i++) {
+            if ($this->book_array[$i] > 0) {
                 $counttypes++;
             }
         }
         return $counttypes;
     }
-    private function defaulPrice()
+    private function default_Price()
     {
         $price = 0;
-        for ($i=1;$i<=count($this->set_of_book);$i++) {
-            $price += $this->set_of_book[$i]*self::PRICE_BOOK;
+        for ($i=1;$i<=count($this->book_array);$i++) {
+            $price += $this->book_array[$i]*self::PRICE_BOOK;
         }
         return $price;
     }
-    
-
-    // public function pieceBiggestDiscount($buybook)
-    // {
-    //     $piecediscount = 0;
-    //     $totaldiscount = '';
-
-    //     $this->valuetypes = array_count_values($buybook);
-    //     $piece = $this->totalPieceDefault();
-        
-    //     $counttypes = $this->countTypes();
-        
-    //     for (; $counttypes> 1; $counttypes = $this->countTypes()) {
-    //         $totaldiscount .= (string)$counttypes;
-    //         $piecediscount += $counttypes * self::PIECE_BOOK * self::DISCOUNT[$counttypes]; //= 10
-    //         $this->updateValueTypes($counttypes);
-    //     }
-
-    //     if (count($this->valuetypes) == 5) {
-    //         $threediscount = $this->countDiscount($totaldiscount, 3);
-    //         $fivediscount = $this->countDiscount($totaldiscount, 5);
-    //         $bountdiscount = min($threediscount,$fivediscount);
-    //         $piecediscount += $bountdiscount*0.4;
-    //     }
-
-    //     return (double)$piece - $piecediscount;
-    // }
-
-    // public function totalPieceDefault()
-    // {
-    //     $piece = 0;
-    //     for ($i = 1; $i <= count($this->valuetypes); $i++) {
-    //         $piece += self::PIECE_BOOK * $this->valuetypes[$i];
-    //     }
-    //     return $piece;
-    // }
-
-    // public function countDiscount($totaldiscount, $searchdiscount)
-    // {
-    //     $totalsearch = 0;
-    //     for ($i = 0; $i < strlen($totaldiscount); $i++) {
-    //         if ($totaldiscount[$i] == $searchdiscount) {
-    //             $totalsearch++;
-    //         }
-    //     }
-    //     return $totalsearch;
-    // }
-
-    // public function countTypes()
-    // {
-    //     $counttypes = 0;
-    //     for ($i = 1; $i <= count($this->valuetypes); $i++) {
-    //         if ($this->valuetypes[$i] > 0) {
-    //             $counttypes++;
-    //         }
-    //     }
-    //     return $counttypes;
-    // }
-
-    // public function updateValueTypes($discounttypes)
-    // {
-    //     for ($i = 1; $i <= count($this->valuetypes);  $i++) {
-    //         if ($this->valuetypes[$i] >= 1) {
-    //             $this->valuetypes[$i]--;
-    //             $discounttypes--;
-    //         }
-    //     }
-    // }
+    public function count_Discount_Times($totaldiscount, $searchdiscount)
+    {
+        $totalsearch = 0;
+        for ($i = 0; $i < count($totaldiscount); $i++) {
+            if ($totaldiscount[$i] == $searchdiscount) {
+                $totalsearch++;
+            }
+        }
+        return $totalsearch;
+    }
 }
